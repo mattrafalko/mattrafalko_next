d5670127e6362860967b816af5b2bd68efaa3329
mattrafalko <mattrafalko@gmail.com>
2021-02-08 09:55:31 -0600

    changed env name variables

--- context/UserData.js
+++ context/UserData.js
@@ -2,9 +2,12 @@ import axios from 'axios';
 
 export const getGithubUserData = async () => {
   try {
-    return await axios.get(
-      `https://api.github.com/users/mattrafalko?client_id=${process.env.REACT_APP_GITHUB_CLIENTID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENTSECRET}`
-    );
+    return await axios.get(`https://api.github.com/users/mattrafalko`, {
+      auth: {
+        clientID: process.env.NEXT_PUBLIC_GITHUB_CLIENTID,
+        secret: process.env.NEXT_PUBLIC_GITHUB_CLIENTSECRET,
+      },
+    });
   } catch (e) {
     return e;
   }
@@ -14,7 +17,13 @@ export const getGithubProjectdata = async () => {
   let projects = [];
   try {
     projects = await axios.get(
-      `https://api.github.com/users/mattrafalko/repos?per_page=50&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENTID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENTSECRET}`
+      `https://api.github.com/users/mattrafalko/repos?per_page=50&sort=created:asc`,
+      {
+        auth: {
+          clientID: process.env.NEXT_PUBLIC_GITHUB_CLIENTID,
+          secret: process.env.NEXT_PUBLIC_GITHUB_CLIENTSECRET,
+        },
+      }
     );
     projects = projects.data.filter((x) => x.fork === false);
   } catch (e) {
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    domains: ['avatars.githubusercontent.com'],
+    domains: ['avatars.githubusercontent.com', 'cdn.sanity.io'],
   },
   webpack(config) {
     config.module.rules.push({
